5a363e966a78ac4e03bbae68e58f1471a2307177
Sahil <sahilashokjagdale@gmail.com>
2025-03-10 16:02:19 +0530

    init commit

---
+++ Java Intellij/Collection_Framework/src/Array_00/Array_00.java
@@ -0,0 +1,14 @@
+package Array_00;
+
+public class Array_00 {
+    public static void main(String[]args) {
+        int arr[] = {15,1,5,6,58,9,5};
+        for(int i=0; i<=6; i++) {
+            System.out.print(arr[i]);
+        }
+
+        int arr1 = new arr1[5];
+        arr1[0] = 15;
+        System.out.println(arr1);
+    }
+}
